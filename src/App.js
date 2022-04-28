29f0e67c560b3998c36c166645f159035b538951
Utkarsh Dwivedi <68611209+Utkarsh742@users.noreply.github.com>
2022-04-28 09:48:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+import Statewise from './Statewise';
+import Footer from './Footer';
+
+
+const App = () => {
+  return(
+    <>
+     <Statewise/>
+  {/* <Contact/> */}
+     <Footer/>
+    </>
+
+  )
+}
+
+export default App;
